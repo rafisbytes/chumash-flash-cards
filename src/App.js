dedc87bed98b081100106315dfbb40d48113c5b6
rafisbytes <yoyorafo@gmail.com>
2022-12-16 11:43:34 -0500

    Cleaned the code

--- src/App.js
+++ src/App.js
@@ -1,20 +1,32 @@
 import './App.css';
 import React, { useState } from "react";
+function DeckPar(props) {    
+    return (
+        <div>
+            <h2>Create a deck</h2>
+            <input placeholder="Name of deck"/>
+            <button onClick={() => props.setDeckOpen("visible")}>Create deck</button>
+        </div>	
+    )
+}
+function FlashcardPar(props) {
+    return (
+        <div {...props}>
+            <h2>Create a flashcard:</h2>
+            <input className="parameter-inputs" placeholder="Original Word" dir="rtl"/>
+            <input className="parameter-inputs" placeholder="Translation"/>
+            <br/>
+            <input className="parameter-inputs" placeholder="שרש" dir="rtl"/>
+            <input className="parameter-inputs" placeholder="Translation of שרש"/>
+	</div>
+    )
+}
 function App() {
   const [deckOpen, setDeckOpen] = useState("collapse");
   return (
     <div className="App">
-            <h2>Create a deck</h2>
-	    <input placeholder="Name of deck"/>
-	    <button onClick={() => setDeckOpen("visible")}>Create deck</button>
-	<div style={{visibility:deckOpen}}>
-       	<h2>Create a flashcard:</h2>
-        <input className="parameter-inputs" placeholder="Original Word" dir="rtl"/>
-        <input className="parameter-inputs" placeholder="Translation"/>
-	<br/>
-        <input className="parameter-inputs" placeholder="שרש" dir="rtl"/>
-        <input className="parameter-inputs" placeholder="Translation of שרש"/>
-	</div>
+        <DeckPar setDeckOpen={setDeckOpen}/>
+	<FlashcardPar style={{visibility:deckOpen}}/>
     </div>
   );
 }
